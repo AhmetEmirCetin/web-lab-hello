89b695333baff17f04caee91e4a4f7c498ec5654
AhmetEmirCetin <06ahmetemircetin@gmail.com>
2026-02-24 15:35:58 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Ahmet Emir Çetin</p>
+      <p>Ogrenci No: 235542012</p>
+      <p>Bolum: Yazilim Muhendisligi</p>
+    </div>
+  )
+}
+
+export default App
